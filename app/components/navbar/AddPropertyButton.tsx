c3148d467065ae7595814e2f5e4ae710c0a4070c
icegellycream <angeli17figurado@gmail.com>
2025-11-08 17:48:01 +0800

    Code for navbar - Figurado, Angeli Marie L.

---
+++ app/components/navbar/AddPropertyButton.tsx
@@ -0,0 +1,9 @@
+const AddPropertyButton = () => {
+    return (
+        <div className="p-2 cursor-pointer text-sm font-semibold rounded-full hover:bg-gray-100 cursor-pointer border border-gray-300">
+            Djangobnb your home
+        </div>
+    )
+}
+
+export default AddPropertyButton;

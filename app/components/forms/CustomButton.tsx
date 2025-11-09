263e13871f2a5c3a10eaa437a057beb9edac87ce
icegellycream <angeli17figurado@gmail.com>
2025-11-09 13:01:33 +0800

    Detail page for chat - Figurado,Angeli Marie L.

---
+++ app/components/forms/CustomButton.tsx
@@ -0,0 +1,23 @@
+interface CustomButtonProps {
+    label: string;
+    onClick?: () => void;
+    className?: string;
+}
+
+
+const CustomButton: React.FC<CustomButtonProps> = ({
+    label,
+    onClick,
+    className
+}) => {
+    return (
+        <div
+            onClick={onClick}
+            className={`py-4 bg-[#ff385c] hover:bg-[#d50027] text-white text-center rounded-xl transition cursor-pointer ${className || 'w-full'}`}
+            >       
+            {label}
+        </div>
+    )
+}
+
+export default CustomButton;

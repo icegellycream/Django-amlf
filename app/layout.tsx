3e87f669ebc8f6af3adfa57b6f9359bf58440729
icegellycream <angeli17figurado@gmail.com>
2025-11-12 03:12:57 +0800

    Video 2 - Create reusable modal window - Figurado, Angeli Marie L.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
+import Modal from "./components/modals/Modal";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,6 +24,10 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const content = (
+      <p>Gwapa ka? </p>
+  )
+
   return (
     <html lang="en">
       <body
@@ -32,7 +37,6 @@ export default function RootLayout({
         <div className="pt-32">
           {children}
         </div>
-
       </body>
     </html>
   );

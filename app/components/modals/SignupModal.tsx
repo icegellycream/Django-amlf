a0131e313450cf70148c3a0cc447f29620de6048
icegellycream <angeli17figurado@gmail.com>
2025-11-16 20:33:06 +0800

    Video 2 - Sign up modal(remember error codes) - Figurado, Angeli Marie L.

---
+++ app/components/modals/SignupModal.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import Modal from "./Modal";
+
+import { useState } from "react";
+import useSignupModal from "@/app/hooks/useSignupModal";
+import CustomButton from "../forms/CustomButton";
+
+const SignupModal = () => {
+    const SignupModal = useSignupModal()
+
+    const content = (
+        <>
+            <form className="space-y-4">
+                <input placeholder="Your e-mail address" type="email" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl"/>
+
+                <input placeholder="Your password" type="password" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl"/>
+
+                <input placeholder="Repeat password" type="password" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl"/>
+
+                <div className="p-5 bg-airbnb text-white rounded-xl opacity-80">
+                    The error message
+                </div>
+                
+                <CustomButton
+                    label="Submit"
+                    onClick={() => console.log('Test')}
+                />
+            </form>
+        </>
+    )
+
+    return (
+        <Modal
+            isOpen={SignupModal.isOpen}
+            close={SignupModal.close}
+            label="Signup in"
+            content={content}
+        />
+    )
+}
+
+export default SignupModal;
